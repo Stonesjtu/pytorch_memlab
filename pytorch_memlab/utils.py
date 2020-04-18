87d4357e8fe652b150a836a21ab3c32745fbacf8
Andrew Jones <andyjones.ed@gmail.com>
2020-04-18 11:28:58 +0000

    Reorders util imports

--- pytorch_memlab/utils.py
+++ pytorch_memlab/utils.py
@@ -1,5 +1,5 @@
-from calmsize import size as calmsize
 from math import isnan
+from calmsize import size as calmsize
 
 def readable_size(num_bytes):
     return '' if isnan(num_bytes) else '{:.2f}'.format(calmsize(num_bytes))
