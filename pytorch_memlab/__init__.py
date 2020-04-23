036b152fa9874051556c2a8b3b3ac395ba13979c
Kaiyu Shi <skyisno.1@gmail.com>
2020-04-23 19:57:16 +0800

    Fix ipython extension import path

--- pytorch_memlab/__init__.py
+++ pytorch_memlab/__init__.py
@@ -1,7 +1,4 @@
 from .courtesy import Courtesy
 from .mem_reporter import MemReporter
 from .line_profiler import LineProfiler, profile, profile_every, set_target_gpu, clear_global_line_profiler
-try:
-    from .extension import load_ipython_extension
-except ImportError:
-    pass
+from .line_profiler.extension import load_ipython_extension
--- pytorch_memlab/line_profiler/__init__.py
+++ pytorch_memlab/line_profiler/__init__.py
@@ -1,3 +1,2 @@
 from .line_profiler import LineProfiler
 from .profile import profile, profile_every, set_target_gpu, clear_global_line_profiler
-from .extension import load_ipython_extension
