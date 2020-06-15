ff71e7300279518a3be01ce96ee1e6b5e41863a7
Kaiyu Shi <skyisno.1@gmail.com>
2020-06-15 19:02:08 +0800

    Bump version

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ except FileNotFoundError:
 
 setup(
     name='pytorch_memlab',
-    version='0.1.0',
+    version='0.2.0',
     licence='MIT',
     description='A lab to do simple and accurate memory experiments on pytorch',
     long_description=long_description,
