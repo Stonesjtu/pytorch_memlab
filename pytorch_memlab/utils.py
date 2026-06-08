a5d15bd955195addf706ced562c358c3b08636e9
Kaiyu Shi <kaiyu@bytedance.com>
2026-06-08 13:40:57 +0800

    Handle symbolic sizes in memory reporter

--- pytorch_memlab/utils.py
+++ pytorch_memlab/utils.py
@@ -2,4 +2,14 @@ from math import isnan
 from calmsize import size as calmsize
 
 def readable_size(num_bytes: int) -> str:
-    return '' if isnan(num_bytes) else '{:.2f}'.format(calmsize(num_bytes))
+    try:
+        if isnan(num_bytes):
+            return ''
+    except TypeError:
+        pass
+
+    size = calmsize(num_bytes)
+    try:
+        return '{:.2f}'.format(size)
+    except TypeError:
+        return str(size)
